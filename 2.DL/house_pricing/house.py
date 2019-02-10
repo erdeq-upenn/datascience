01d928be826f22d103f9eb96259e66e7277036fa
Dr E <erdeq.upenn@gmail.com>
2019-02-10 00:06:56 -0500

    -m

--- 2.DL/house_pricing/house.py
+++ 2.DL/house_pricing/house.py
@@ -1,1 +1,14 @@
-lll
+# edited by Dequan Er
+
+# %matplotlib inline
+import d2lzh as d2l
+from mxnet import autograd, gluon, init, nd
+from mxnet.gluon import data as gdata, loss as gloss, nn
+import numpy as np
+import pandas as pd
+
+
+train_data = pd.read_csv('data/train.csv')
+test_data = pd.read_csv('data/test.csv')
+
+print(train_data.shape)
