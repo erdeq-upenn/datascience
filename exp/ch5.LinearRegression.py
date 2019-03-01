7226655ab36956a8b8ad8db4b271856980c7490c
Dr E <erdeq.upenn@gmail.com>
2019-02-28 22:06:22 -0500

    SVM and BicyclePridicter

--- exp/ch5.LinearRegression.py
+++ exp/ch5.LinearRegression.py
@@ -91,45 +91,118 @@ class GaussianFeatures(BaseEstimator, TransformerMixin):
         return self._gauss_basis(X[:, :, np.newaxis], self.centers_,
                                  self.width_, axis=1)
     
-gauss_model = make_pipeline(GaussianFeatures(20),
-                            LinearRegression())
-gauss_model.fit(x[:, np.newaxis], y)
-yfit = gauss_model.predict(xfit[:, np.newaxis])
-
-plt.scatter(x, y)
-plt.plot(xfit, yfit)
-plt.xlim(0, 10);
+#gauss_model = make_pipeline(GaussianFeatures(20),
+#                            LinearRegression())
+#gauss_model.fit(x[:, np.newaxis], y)
+#yfit = gauss_model.predict(xfit[:, np.newaxis])
+#
+#plt.scatter(x, y)
+#plt.plot(xfit, yfit)
+#plt.xlim(0, 10);
 
 
 # plot overfitting bases
 
-def basis_plot(model, title=None):
-    fig, ax = plt.subplots(2, sharex=True)
-    model.fit(x[:, np.newaxis], y)
-    ax[0].scatter(x, y)
-    ax[0].plot(xfit, model.predict(xfit[:, np.newaxis]))
-    ax[0].set(xlabel='x', ylabel='y', ylim=(-1.5, 1.5))
+#def basis_plot(model, title=None):
+#    fig, ax = plt.subplots(2, sharex=True)
+#    model.fit(x[:, np.newaxis], y)
+#    ax[0].scatter(x, y)
+#    ax[0].plot(xfit, model.predict(xfit[:, np.newaxis]))
+#    ax[0].set(xlabel='x', ylabel='y', ylim=(-1.5, 1.5))
+#    
+#    if title:
+#        ax[0].set_title(title)
+#
+#    ax[1].plot(model.steps[0][1].centers_,
+#               model.steps[1][1].coef_)
+#    ax[1].set(xlabel='basis location',
+#              ylabel='coefficient',
+#              xlim=(0, 10))
+#    
+#model = make_pipeline(GaussianFeatures(30), LinearRegression())
+#basis_plot(model)
+#
+#
+## add L2 regularization known as Ridge
+#from sklearn.linear_model import Ridge
+#model = make_pipeline(GaussianFeatures(30), Ridge(alpha=0.1))
+#basis_plot(model, title='Ridge Regression')
+#
+#
+## add L1 regularizaiotn known as Lasso
+#from sklearn.linear_model import Lasso
+#model = make_pipeline(GaussianFeatures(30), Lasso(alpha=0.001))
+#basis_plot(model, title='Lasso Regression')
+
+
+##  predict bicycle traffic
+
+import pandas as pd
+counts = pd.read_csv('FremontBridge.csv', index_col='Date', parse_dates=True)
+weather = pd.read_csv('BicycleWeather.csv', index_col='DATE', parse_dates=True)
+
+daily = counts.resample('d').sum()
+daily['Total'] = daily.sum(axis=1)
+daily = daily[['Total']] # remove other columns
+
+days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+for i in range(7):
+    daily[days[i]] = (daily.index.dayofweek == i).astype(float)
     
-    if title:
-        ax[0].set_title(title)
+from pandas.tseries.holiday import USFederalHolidayCalendar
+cal = USFederalHolidayCalendar()
+holidays = cal.holidays('2012', '2016')
+daily = daily.join(pd.Series(1, index=holidays, name='holiday'))
+daily['holiday'].fillna(0, inplace=True)
 
-    ax[1].plot(model.steps[0][1].centers_,
-               model.steps[1][1].coef_)
-    ax[1].set(xlabel='basis location',
-              ylabel='coefficient',
-              xlim=(0, 10))
+def hours_of_daylight(date, axis=23.44, latitude=47.61):
+    """Compute the hours of daylight for the given date"""
+    days = (date - pd.datetime(2000, 12, 21)).days
+    m = (1. - np.tan(np.radians(latitude))
+         * np.tan(np.radians(axis) * np.cos(days * 2 * np.pi / 365.25)))
+    return 24. * np.degrees(np.arccos(1 - np.clip(m, 0, 2))) / 180.
+
+daily['daylight_hrs'] = list(map(hours_of_daylight, daily.index))
+daily[['daylight_hrs']].plot()
+plt.ylim(8, 17)
+
+# temperatures are in 1/10 deg C; convert to C
+weather['TMIN'] /= 10
+weather['TMAX'] /= 10
+weather['Temp (C)'] = 0.5 * (weather['TMIN'] + weather['TMAX'])
+
+# precip is in 1/10 mm; convert to inches
+weather['PRCP'] /= 254
+weather['dry day'] = (weather['PRCP'] == 0).astype(int)
+
+daily = daily.join(weather[['PRCP', 'Temp (C)', 'dry day']])
+
+daily['annual'] = (daily.index - daily.index[0]).days / 365.
+
+daily.head()
+
+# Drop any rows with null values
+daily.dropna(axis=0, how='any', inplace=True)
+
+column_names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'holiday',
+                'daylight_hrs', 'PRCP', 'dry day', 'Temp (C)', 'annual']
+X = daily[column_names]
+y = daily['Total']
+
+model = LinearRegression(fit_intercept=False)
+model.fit(X, y)
+daily['predicted'] = model.predict(X)
+
+# plot 
+daily[['Total', 'predicted']].plot(alpha=0.5);
+
+params = pd.Series(model.coef_, index=X.columns)
+
+from sklearn.utils import resample
+np.random.seed(1)
+err = np.std([model.fit(*resample(X, y)).coef_
+              for i in range(1000)], 0)
+
+print(pd.DataFrame({'effect': params.round(0),
+                    'error': err.round(0)}))
     
-model = make_pipeline(GaussianFeatures(30), LinearRegression())
-basis_plot(model)
-
-
-# add L2 regularization known as Ridge
-from sklearn.linear_model import Ridge
-model = make_pipeline(GaussianFeatures(30), Ridge(alpha=0.1))
-basis_plot(model, title='Ridge Regression')
-
-
-# add L1 regularizaiotn known as Lasso
-from sklearn.linear_model import Lasso
-model = make_pipeline(GaussianFeatures(30), Lasso(alpha=0.001))
-basis_plot(model, title='Lasso Regression')
