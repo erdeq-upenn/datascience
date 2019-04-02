1a06383bc50085df8eb928155319b77fb23a4413
Dr E <erdeq.upenn@gmail.com>
2019-04-02 15:20:57 -0400

    1

--- exp/pandas_make_df.py
+++ exp/pandas_make_df.py
@@ -12,3 +12,11 @@ def make_df(cols,ind):
     data = {c:[str(c) +str(i) for i in ind] for c in cols}
     return pd.DataFrame(data,ind)
 
+# Following are shell commands to download the data
+# !curl -O https://raw.githubusercontent.com/jakevdp/data-USstates/master/state-population.csv
+# !curl -O https://raw.githubusercontent.com/jakevdp/data-USstates/master/state-areas.csv
+# !curl -O https://raw.githubusercontent.com/jakevdp/data-USstates/master/state-abbrevs.csv
+    
+pop = pd.read_csv('state-population.csv')
+areas = pd.read_csv('state-areas.csv')
+abbrevs = pd.read_csv('state-abbrevs.csv')
