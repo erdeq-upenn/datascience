e9bba7cf4010baeb01cdaec74efae33e15b9dd65
Dr E <erdeq.upenn@gmail.com>
2019-05-06 23:02:49 -0400

    a

--- exp/animation.py
+++ exp/animation.py
@@ -1,64 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d.axes3d as p3
-import matplotlib.animation as animation
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
+from matplotlib import animation, rc
+from IPython.display import HTML, Image
 
 
-def Gen_RandLine(length, dims=2):
-    """
-    Create a line using a random walk algorithm
+rc('animation', html='html5')
 
-    length is the number of points for the line.
-    dims is the number of dimensions the line has.
-    """
-    lineData = np.empty((dims, length))
-    lineData[:, 0] = np.random.rand(dims)
-    for index in range(1, length):
-        # scaling the random numbers by 0.1 so
-        # movement is small compared to position.
-        # subtraction by 0.5 is to change the range to [-0.5, 0.5]
-        # to allow a line to move backwards.
-        step = ((np.random.rand(dims) - 0.5) * 0.1)
-        lineData[:, index] = lineData[:, index - 1] + step
+# First set up the figure, the axis, and the plot element we want to animate
+fig, ax = plt.subplots()
 
-    return lineData
+ax.set_xlim(( 0, 2))
+ax.set_ylim((-2, 2))
 
+line, = ax.plot([], [], lw=2)
 
-def update_lines(num, dataLines, lines):
-    for line, data in zip(lines, dataLines):
-        # NOTE: there is no .set_data() for 3 dim data...
-        line.set_data(data[0:2, :num])
-        line.set_3d_properties(data[2, :num])
-    return lines
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return (line,)
 
-# Attaching 3D axis to the figure
-fig = plt.figure()
-ax = p3.Axes3D(fig)
+# animation function. This is called sequentially
+def animate(i):
+    x = np.linspace(0, 2, 1000)
+    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    return (line,)
 
-# Fifty lines of random 3-D lines
-data = [Gen_RandLine(25, 3) for index in range(50)]
+# call the animator. blit=True means only re-draw the parts that 
+# have changed.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=100, interval=20, blit=True)
 
-# Creating fifty line objects.
-# NOTE: Can't pass empty arrays into 3d version of plot()
-lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in data]
-
-# Setting the axes properties
-ax.set_xlim3d([0.0, 1.0])
-ax.set_xlabel('X')
-
-ax.set_ylim3d([0.0, 1.0])
-ax.set_ylabel('Y')
-
-ax.set_zlim3d([0.0, 1.0])
-ax.set_zlabel('Z')
-
-ax.set_title('3D Test')
-
-# Creating the Animation object
-line_ani = animation.FuncAnimation(fig, update_lines, 25, fargs=(data, lines),
-                                   interval=50, blit=False)
-
-plt.show()
+anim.save('~/Desktop/animation.gif', writer='imagemagick', fps=60)
